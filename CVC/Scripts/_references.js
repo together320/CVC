e988a4eb055a0e32f8f701f5f49e681faed37271
together320 <yuzheli320@gmail.com>
2023-09-15 08:56:21 +0800

    Revert "Revert "CVC # first version""
    This reverts commit cef05838703b80b745e9407941726211ef110dd5.

--- CVC/Scripts/_references.js
+++ CVC/Scripts/_references.js
@@ -1,4 +1,5 @@
-﻿/// <reference path="jquery-3.1.1.js" />
+﻿/// <reference path="modernizr-2.8.3.js" />
+/// <reference path="jquery-3.3.1.js" />
 /// <reference path="jquery-ui-1.12.1.js" />
 /// <autosync enabled="false" />
 /// <reference path="serenity/Serenity.CoreLib.js" />
